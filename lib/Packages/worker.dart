4a14163e9624b72621942b44d08fbb5714c30d80
alizahid04 <alizahid.272004@gmail.com>
2025-07-24 20:23:22 +0500

    Use dotenv for API key security

--- lib/Packages/worker.dart
+++ lib/Packages/worker.dart
@@ -1,60 +1,59 @@
-import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:http/http.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-final apiKey = dotenv.env['OPENWEATHER_API_KEY'];
-class Worker {
- late String location;
-  Worker({String? loc}){
-    location = loc ?? 'Faisalabad';
-  }
-  String temp = '';
-  String humidity = '';
-  String airSpeed = '';
-  String description = '';
-  String main = '';
-  String icon = '';
+  import 'dart:convert';
+  import 'package:http/http.dart';
+  import 'package:flutter_dotenv/flutter_dotenv.dart';
+  class Worker {
+   late String location;
+    Worker({String? loc}){
+      location = loc ?? 'Faisalabad';
+    }
+    String temp = '';
+    String humidity = '';
+    String airSpeed = '';
+    String description = '';
+    String main = '';
+    String icon = '';
 
-  Future <void> getData() async {
-    try{
-      Response response = await get(
-        Uri.parse(
-          "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=$apiKey&units=metric",
-        ),
-      );
-      Map data = jsonDecode(response.body);
+    Future <void> getData() async {
+      try{
+        final apiKey = dotenv.env['OPENWEATHER_API_KEY'];
+        Response response = await get(
+          Uri.parse(
+            "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=$apiKey&units=metric",
+          ),
+        );
+        Map data = jsonDecode(response.body);
 
-      // getting main map , temp and humidity
-      Map temp_data = data['main'];
-      double getTemp = temp_data['temp'];
-      String getHumidity = temp_data['humidity'].toString();
+        // getting main map , temp and humidity
+        Map temp_data = data['main'];
+        double getTemp = temp_data['temp'];
+        String getHumidity = temp_data['humidity'].toString();
 
-      // getting air map and speed
-      Map wind = data['wind'];
-      double getAir_speed = wind['speed'] * 3.6;
+        // getting air map and speed
+        Map wind = data['wind'];
+        double getAir_speed = wind['speed'] * 3.6;
 
-      // weather is in list
-      // getting map from weather list and then main and descrtption
-      List weather_data = data['weather'];
-      Map weather_main_data = weather_data[0];
-      String getMain_des = weather_main_data['main'];
-      String getDesc = weather_main_data['description'];
-      String get_Icon = weather_main_data['icon'].toString();
+        // weather is in list
+        // getting map from weather list and then main and descrtption
+        List weather_data = data['weather'];
+        Map weather_main_data = weather_data[0];
+        String getMain_des = weather_main_data['main'];
+        String getDesc = weather_main_data['description'];
+        String get_Icon = weather_main_data['icon'].toString();
 
-      //Assiging
-      temp = getTemp.toString();
-      humidity = getHumidity;
-      airSpeed = getAir_speed.toString();
-      description = getDesc;
-      main = getMain_des;
-      icon = get_Icon;
-    }catch(e){
-      temp = 'Error';
-      humidity = 'N/A';
-      airSpeed = 'N/A';
-      description = 'Unable to fetch';
-      main = 'N/A';
-      icon = '01d';
+        //Assiging
+        temp = getTemp.toString();
+        humidity = getHumidity;
+        airSpeed = getAir_speed.toString();
+        description = getDesc;
+        main = getMain_des;
+        icon = get_Icon;
+      }catch(e){
+        temp = 'Error';
+        humidity = 'N/A';
+        airSpeed = 'N/A';
+        description = 'Unable to fetch';
+        main = 'N/A';
+        icon = '01d';
+      }
     }
   }
-}
--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:thermacast/Pages/Loading.dart';
 import 'package:thermacast/Pages/home.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await dotenv.load();
+
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
-      statusBarColor: Color(0xFF2193b0), // Dark sky blue
-      statusBarIconBrightness: Brightness.light, // use dark icons for light background
+      statusBarColor: Color(0xFF2193b0),
+      statusBarIconBrightness: Brightness.light,
     ),
   );
 
-  {
-    runApp(MaterialApp(
-      routes: {
-        "/": (context) => loading(),
-        "/home": (context) => home(),
-        "/loading": (context) => loading(),
-      },
-    ));
-  }
+  runApp(MaterialApp(
+    routes: {
+      "/": (context) => loading(),
+      "/home": (context) => home(),
+      "/loading": (context) => loading(),
+    },
+  ));
 }
